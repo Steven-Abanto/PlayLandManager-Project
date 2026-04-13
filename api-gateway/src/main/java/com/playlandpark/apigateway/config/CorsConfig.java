5829c5c848e23a74db36ce3f7f5f3cbe27432a6b
Steven-Abanto <steven.abanto.xd@gmail.com>
2026-04-12 20:40:24 -0500

    Se configura cors para front end

--- api-gateway/src/main/java/com/playlandpark/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/playlandpark/apigateway/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.playlandpark.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.*;
 
@@ -15,8 +16,20 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:3000"
+        ));
+
+        config.setAllowedMethods(List.of(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.OPTIONS.name()
+        ));
+
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
